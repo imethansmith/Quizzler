50a965a76482df0806d13bda4e22d343f57b9db8
Ethan Smith <edhsmith@icloud.com>
2021-02-16 20:49:47 +1300

    Refactored code into MVC design. Performed basic bug testing without unit tests.

---
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,91 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var restartButton: UIButton!
+    
+    var quizBrain = QuizBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Create initial progress bar
+        progressBar.progress = quizBrain.progressPerQuestion
+        
+        // Setup first question
+        updateUI(nextQuestion: quizBrain.quiz[0].text)
+    }
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        // Assign binary conversion on answer given
+        let answer = { () -> Int in
+            if (sender.currentTitle == "True") {
+                return 1
+            } else {
+                return 0
+            }
+        }()
+        
+        // Pass result to model, update View with result
+        if quizBrain.checkAnswer(answer) {
+            sender.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+            UIView.animate(withDuration: 0.8) {
+                sender.backgroundColor = UIColor.clear
+            }
+        } else {
+            sender.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+            UIView.animate(withDuration: 0.8) {
+                sender.backgroundColor = UIColor.clear
+            }
+        }
+        
+        // Update UI with next question, or display final result
+        updateUI(nextQuestion: quizBrain.getNextQuestion())
+        
+        // Quiz completion: Update look of app, hide buttons until restart
+        if (quizBrain.questionNumber == 0) {
+            progressBar.progress = 1.0
+            progressBar.progressTintColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
+            trueButton.isHidden = true
+            falseButton.isHidden = true
+            restartButton.isHidden = false
+        }
+    }
+    
+    func updateUI(nextQuestion: String) {
+        // Update current question on main Label
+        questionLabel.text = (nextQuestion)
+        
+        // Update progress bar
+        progressBar.progress = quizBrain.progressPerQuestion * Float(quizBrain.questionNumber)
+    }
+    
+    @IBAction func playAgain(_ sender: UIButton) {
+        // Reset values
+        quizBrain.resetGame()
+        
+        // Reset UI
+        progressBar.progressTintColor = #colorLiteral(red: 1, green: 0.4588235294, blue: 0.6588235294, alpha: 1)
+        progressBar.progress = quizBrain.progressPerQuestion
+        
+        // Show buttons
+        trueButton.isHidden = false
+        falseButton.isHidden = false
+        
+        // Restart views
+        restartButton.isHidden = true
+        updateUI(nextQuestion: quizBrain.quiz[0].text)
+    }
+}
+
---
+++ Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,19 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Ethan Smith on 16/02/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: Int
+    
+    init(q: String, a: Int) {
+        text = q
+        answer = a
+    }
+}
